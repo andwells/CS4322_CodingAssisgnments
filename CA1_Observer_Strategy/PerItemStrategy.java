9a8783e3797aebc5d3d2a8f08c04283f7b2211ab
leeadams19 <leeadams19@gmail.com>
2015-02-01 15:03:21 -0500

    Updated logic for perItemStrategy

--- CA1_Observer_Strategy/PerItemStrategy.java
+++ CA1_Observer_Strategy/PerItemStrategy.java
@@ -1,7 +1,38 @@
-public class PerItemStrategy implements SalesStrategy, SalesStrategy {
+import java.io.File
+import java.util.Scanner
 
-  public SalesStats Read() {
-  return null;
+public class PerItemStrategy implements SalesStrategy
+{
+
+  public SalesStats Read(File toRead)
+  {
+    
+    Scanner reader;
+    try
+    {
+      reader = new Scanner(toRead)
+    }
+    catch (FileNotFoundException fnEx)
+    {
+      return null;
+    }
+    
+    double itemSum = 0;
+    String item;
+    ArrayList<Double> orders = new ArrayList<Double>();
+    while(reader.hasNext())
+    {
+      item = reader.next();
+      if(item.equals("#"))
+      {
+        orders.Add(itemSum)
+        itemSum = 0;
+      }
+      else
+        itemSum += Double.Parse(item);
+    }
+  
+  return new SalesStats(orders);
+  
   }
-
 }
