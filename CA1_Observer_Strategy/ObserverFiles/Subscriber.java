d89caea5e7cddec632172f7bcb5873dc9ad470fb
andwells <andwells@valdosta.edu>
2015-02-02 20:58:24 -0500

    Organized Files into Folders

--- CA1_Observer_Strategy/ObserverFiles/Subscriber.java
+++ CA1_Observer_Strategy/ObserverFiles/Subscriber.java
@@ -3,13 +3,16 @@ import java.io.*;
 import java.net.*;
 import javax.swing.*;
 import java.awt.*;
-public class Subscriber extends JFrame implements Subscription, java.io.Serializable
+public class Subscriber extends JFrame implements Subscription, Serializable
 {
    private String subscriberName;
    private Socket socket;
    
-   public Subscriber()
+   public Subscriber(Socket remoteSocket)
    {
+      super("News Reader");
+      this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+      this.setSize(700, 350);
       //buildUI
       /*do connection
       socket = new Socket(localhost, 8080);
@@ -17,6 +20,7 @@ public class Subscriber extends JFrame implements Subscription, java.io.Serializ
       */
       //register with server
       //Subscript
+      this.setVisible(true);
    }
    
    public void update(Object arg)
@@ -26,6 +30,6 @@ public class Subscriber extends JFrame implements Subscription, java.io.Serializ
    
    private void buildUI()
    {
-      
+           
    }
 }
