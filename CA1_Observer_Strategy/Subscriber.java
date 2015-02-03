be6aeddae1e6bc15ece9c33a736a88135d7a74c3
andwells <andwells@valdosta.edu>
2015-02-02 19:25:27 -0500

    Minor Changes to Subscriber

--- CA1_Observer_Strategy/Subscriber.java
+++ CA1_Observer_Strategy/Subscriber.java
@@ -1,8 +1,31 @@
-public class Subscriber implements Subscription {
-
-  private String subscriberName;
-
-  public void update(Object arg) {
-  }
-
+import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+public class Subscriber extends JFrame implements Subscription, java.io.Serializable
+{
+   private String subscriberName;
+   private Socket socket;
+   
+   public Subscriber()
+   {
+      //buildUI
+      /*do connection
+      socket = new Socket(localhost, 8080);
+      socket.open();
+      */
+      //register with server
+      //Subscript
+   }
+   
+   public void update(Object arg)
+   {
+      
+   }
+   
+   private void buildUI()
+   {
+      
+   }
 }
---
+++ CA1_Observer_Strategy/SubscriberDriver.java
@@ -0,0 +1,7 @@
+public class SubscriberDriver
+{
+   public static void main(String[] args)
+   {
+      new Subscriber();
+   }
+}
