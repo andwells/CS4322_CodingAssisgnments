ec15a3b65a37c1feb720feebb95188b36303a9ab
leeadams19 <leeadams19@gmail.com>
2015-02-03 20:28:16 -0500

    Logic for Content Provider Updated Needs Extension

--- CA1_Observer_Strategy/ObserverFiles/ContentProvider.java
+++ CA1_Observer_Strategy/ObserverFiles/ContentProvider.java
@@ -1,25 +1,60 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ContentProvider implements Content
 {
    private Integer notifyThreshold;   
    private ArrayList<Subscription> subscribers;
+   private ArrayList<String> newsBuffer;
+   private Scanner scan;
    
    public void setThreshold(Integer newThreshold)
    {
+      if(newThreshold>0)
+         notifyThreshold = newThreshold;
    }
    
    public void notifySubscribers()
    {
+      for(Subscription sub : subscribers)
+         sub.Update(null);
    }
    
    public Boolean subscribe(Subscription aSubscription)
    {
-       return null;
+      subscribers.add(aSubscription);
+      return true;
    }
    
    public Boolean unsubscribe(Subscription toRemove)
    {
-       return null;
+      if(subscribers.contains(toRemove)
+      {
+         subscribers.remove(toRemove);
+         return true;
+      }
+      else
+      {
+         return false;
+      }
    }
-
+   
+   public ContentProvider() throws IOException
+   {
+         notifyThreshold = 10;
+         subscribers = new ArrayList<Subscription>;
+         newsBuffer = new ArrayList<String>;
+         scan = new Scanner(new File("news.txt"));
+   }
+   
+   public void readFromFile() 
+   {
+      if(scan.hasNext())
+      {
+         newsBuffer.add(scan.NextLine());
+         if(newsBuffer.size()>=notifyThreshold)
+            notifySubscribers();
+      }
+   }
+   
 }
