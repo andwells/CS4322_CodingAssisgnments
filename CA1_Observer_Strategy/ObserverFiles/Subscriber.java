23b01efb303c785d8c5de82befafa8825252fc08
andwells <andwells@valdosta.edu>
2015-02-06 16:23:50 -0500

    More Subscriber Changes

--- CA1_Observer_Strategy/ObserverFiles/Subscriber.java
+++ CA1_Observer_Strategy/ObserverFiles/Subscriber.java
@@ -1,35 +1,109 @@
 import java.util.ArrayList;
-import java.io.*;
-import java.net.*;
-import javax.swing.*;
-import java.awt.*;
-public class Subscriber extends JFrame implements Subscription, Serializable
+import java.util.Collections;
+import java.util.Comparator;
+public class Subscriber implements Subscription
 {
    private String subscriberName;
-   private Socket socket;
    
-   public Subscriber(Socket remoteSocket)
+   //Comparators for sorting
+   Comparator<PressRelease> byName;
+   Comparator<PressRelease> byDate;
+   Comparator<PressRelease> byCategory;
+   
+   public Subscriber(String name)
    {
-      super("News Reader");
-      this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-      this.setSize(700, 350);
-      //buildUI
-      /*do connection
-      socket = new Socket(localhost, 8080);
-      socket.open();
-      */
-      //register with server
-      //Subscript
-      this.setVisible(true);
+      subscriberName = name;
+      byName = new byNameComparator();
+      byDate = new byDateComparator();
+      byCategory = new byCategoryComparator();
    }
    
    public void update(Object arg)
-   {
-      
+   {      
+      if(arg instanceof ArrayList)
+      {
+         System.out.printf("\n\nNew updates have been seen by %s! Printing data...\n", this.subscriberName);
+         ArrayList<PressRelease> temp1 = (ArrayList<PressRelease>)arg;
+         Collections.sort(temp1, byName);
+         
+         System.out.println("***\tMovies sorted by Name\t***\n");
+         System.out.printf("Name\t\tRelease Date\t\tCategory\n");
+         for(PressRelease pr : temp1)
+         {
+            System.out.printf("%s\t\t%s\t\t%s", pr.getName(), pr.getReleaseDate(), pr.getCategory());
+         }
+         
+         System.out.println("***\tMovies sorted by Newest Release Year\t***\n");
+         Collections.sort(temp1, byDate);//Sort items by year
+         Collections.reverse(temp1);//Put newest releases first
+         for(PressRelease pr : temp1)
+         {
+            System.out.printf("%s\t\t%s\t\t%s", pr.getName(), pr.getReleaseDate(), pr.getCategory());
+         }
+         
+         System.out.println("***\tMovies sorted by Category\t***\n");
+         Collections.sort(temp1, byCategory);
+         for(PressRelease pr : temp1)
+         {
+            System.out.printf("%s\t\t%s\t\t%s", pr.getName(), pr.getReleaseDate(), pr.getCategory());
+         }
+      }
    }
    
-   private void buildUI()
+   private class byNameComparator implements Comparator<PressRelease>
    {
-           
+      public int compare(PressRelease pr1, PressRelease pr2)
+      {
+         if(pr1.getName().compareTo(pr2.getName()) < 0)
+         {
+            return -1;
+         }
+         else if(pr1.getName().compareTo(pr2.getName()) == 0)
+         {
+            return 0;
+         }
+         else
+         {
+            return 1;
+         }
+      }
+   }
+   
+   private class byDateComparator implements Comparator<PressRelease>
+   {
+      public int compare(PressRelease pr1, PressRelease pr2)
+      {
+         if(pr1.getReleaseDate() < 0)
+         {
+            return -1;
+         }
+         else if(pr1.getReleaseDate() == 0)
+         {
+            return 0;
+         }
+         else
+         {
+            return 1;
+         }
+      }
+   }
+   
+   private class byCategoryComparator implements Comparator<PressRelease>
+   {
+      public int compare(PressRelease pr1, PressRelease pr2)
+      {
+         if(pr1.getCategory().compareTo(pr2.getCategory()) < 0)
+         {
+            return -1;
+         }
+         else if(pr1.getCategory().compareTo(pr2.getCategory()) == 0)
+         {
+            return 0;
+         }
+         else
+         {
+            return 1;
+         }
+      }
    }
 }
--- CA1_Observer_Strategy/ObserverFiles/SubscriberDriver.java
+++ CA1_Observer_Strategy/ObserverFiles/SubscriberDriver.java
@@ -1,7 +1,20 @@
+import java.util.ArrayList;
 public class SubscriberDriver
 {
    public static void main(String[] args)
    {
-      new Subscriber();
+      try
+      {
+         Content cp = new ContentProvider();
+         Subscription s1 = new Subscriber("Peter");
+         Subscription s2 = new Subscriber2("Andrew");
+         cp.subscribe(s1);
+         cp.subscribe(s2);
+         cp.start();
+      }
+      catch(Exception ex)
+      {
+         System.out.println(ex);
+      }
    }
 }
