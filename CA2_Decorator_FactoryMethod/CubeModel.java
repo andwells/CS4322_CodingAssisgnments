c8fc5f0cb3c8060229cb4bad6d3e433bcf20b68e
leeadams19 <leeadams19@gmail.com>
2015-02-07 15:30:44 -0500

    Update CubeModel.java

--- CA2_Decorator_FactoryMethod/CubeModel.java
+++ CA2_Decorator_FactoryMethod/CubeModel.java
@@ -3,4 +3,8 @@ public class CubeModel {
   proctected Function createFunction(String func){
     //logic from decorators
   }
+  
+  public void evalute(String func, double[] xValues){
+    createFunction(func);
+  }
 }
