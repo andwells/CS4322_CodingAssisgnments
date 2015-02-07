515649c920ebe6eab14901d39b54031432b399c1
leeadams19 <leeadams19@gmail.com>
2015-02-07 15:30:09 -0500

    Update Model.java

--- CA2_Decorator_FactoryMethod/Model.java
+++ CA2_Decorator_FactoryMethod/Model.java
@@ -3,8 +3,6 @@ public abstract class Model
   
   protected abstract Function createFunction(String func);
   
-  // other methods here... like evaluate()
-  
   public void evalute(String func, double[] xValues){
     createFunction(func);
   }
