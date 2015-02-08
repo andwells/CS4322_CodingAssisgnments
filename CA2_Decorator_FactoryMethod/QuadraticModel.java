13daba1aa071404bdfff326bbdb5d56e5a4845ec
andwells <andwells@valdosta.edu>
2015-02-08 16:31:21 -0500

    Quadratic Model

--- CA2_Decorator_FactoryMethod/QuadraticModel.java
+++ CA2_Decorator_FactoryMethod/QuadraticModel.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
 public class QuadraticModel{
   
   protected Function createFunction(String func)
   {
-    //logic from decorators
+      char firstVar;
+      ArrayList<Function> parts = new ArrayList<Function>();
+
+      for(int i = 0; i < func.length();i++)
+      {
+         char currentChar = func.charAt(i);
+      	if(!isLegalChar(currentChar))
+      	{
+      		//throw new exception
+      	}
+      	else if(true)
+      	{
+      	}
+      }
+    
+    
     return null;
   }
   
