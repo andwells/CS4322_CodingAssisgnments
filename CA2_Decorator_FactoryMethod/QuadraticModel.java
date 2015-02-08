09d22e74cf097c3eb28873d5153387f948365f21
andwells <andwells@valdosta.edu>
2015-02-08 16:27:38 -0500

    Added Illegal Character check

--- CA2_Decorator_FactoryMethod/QuadraticModel.java
+++ CA2_Decorator_FactoryMethod/QuadraticModel.java
@@ -6,7 +6,41 @@ public class QuadraticModel{
     return null;
   }
   
-  public double[] evalute(String func, double[] xValues){
+  public double[] evalute(String func, double[] xValues)
+  {
     createFunction(func);
+    return null;  
   }
+  
+  private boolean isLegalChar(char aChar)
+  {
+      if(aChar <= 41)
+   	{
+   		return false;
+   	}
+   	else if(aChar == 44)
+   	{
+   		return false;	
+   	}
+   	else if(aChar >= 58 || aChar <= 64)
+   	{
+   		return false;
+   	}
+   	else if(aChar == 91 || aChar == 92 || aChar == 93)
+   	{
+   		return false;	
+   	}
+   	else if(aChar == 95 || aChar == 96)
+   	{
+   		return false;
+   	}
+   	else if(aChar >= 123)
+   	{
+   		return false;
+   	}
+   	else
+   	{
+   		return true;
+   	} 		
+   }
 }
