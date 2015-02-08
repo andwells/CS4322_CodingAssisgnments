e6473451df1301f40b7b7afdc619bc419c0c0c18
leeadams19 <leeadams19@gmail.com>
2015-02-08 16:24:24 -0500

    Update Divide.java

--- CA2_Decorator_FactoryMethod/Divide.java
+++ CA2_Decorator_FactoryMethod/Divide.java
@@ -1,20 +1,3 @@
-/*public class Divide extends Function
-{
-   private Function leftSide;
-   private Function rightSide;
-   
-   public Divide(Function left, Function right)
-   {
-      leftSide = left;
-      rightSide = right;
-   }
-   
-   public double evaluate(double at)
-   {
-      return leftSide.evaluate(at) / rightSide.evaluate(at);
-   }
-}*/
-
 public class Divide extends Function
 {
    private Function leftSide;
@@ -28,17 +11,6 @@ public class Divide extends Function
    
    public double evaluate(double at)
    {
-      double right = evaluateRight(at);
-      
-      if (right != 0)
-         return leftSide.evaluate(at) / right;
-      else
-         // error handle instead of null
-         return null;
-   }
-   
-   public double evaluateRight(double at)
-   {
-      return rightSide.evaluate(at)
+      return leftSide.evaluate(at) / rightSide.evaluate(at);
    }
 }
