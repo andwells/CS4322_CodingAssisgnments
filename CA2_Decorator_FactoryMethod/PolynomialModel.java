cfbd2a8d82999adc7dfc8c5a1345101f34e6170f
andwells <andwells@valdosta.edu>
2015-02-10 12:59:26 -0500

    Added More parsing logic

--- CA2_Decorator_FactoryMethod/PolynomialModel.java
+++ CA2_Decorator_FactoryMethod/PolynomialModel.java
@@ -6,33 +6,66 @@ public class PolynomialModel extends Model
    
    protected Function createFunction(String func)
    {
-      Pattern power = Pattern.compile("(\\p{Digit})*\\p{Alpha}\\^(\\p{Digit})+");
+      Pattern power = Pattern.compile("(\\p{Digit})*\\p{Alpha}\\^(\\p{Digit})");
       Pattern linear = Pattern.compile("\\p{Alpha}((\\+|\\-)(\\p{Digit})+)?");
       Pattern constant = Pattern.compile("^\\b(\\p{Digit})+\\b$");
+      Pattern numbers = Pattern.compile("\\p{Digit}+");
       
       Matcher powerMatch = power.matcher(func);
       Matcher linearMatch = linear.matcher(func);
       Matcher constMatch = constant.matcher(func);
+      Matcher numsMatch = numbers.matcher(func);
       if(powerMatch.find())
       {
          
          String section = powerMatch.group();
          int degree = Integer.parseInt(section.substring(section.indexOf('^')));
          powerMatch.reset();
-         
+         int previousPow = Integer.MIN_VALUE;
          while(powerMatch.find())
          {
             String part = powerMatch.group();
+            String numsFound = "";
+            int number = 1;
+            numsMatch = numbers.matcher(part);
+            if(numsMatch.find())
+            {
+               number = Integer.parseInt(numsMatch.group());
+               if(number >= previousPow)
+               {
+                  //throw malformed excepiton
+               }
+            }
+            int curPow = Integer.parseInt(part.split("^")[1]);
+            Function variable = new Variable();
+            Function exp = new Exponent(variable, curPow);
+            Function mult = new Multiply(new Constant(number), exp);
+            
+            char sign = func.charAt(powerMatch.end());
+            Function plusMinus;
+            if(sign == '+')
+            {
+               //create placeholder for addition
+            }
+            else if(sign == '-')
+            {
+               //create placeholder for subtraction
+            }
+            else
+            {
+               //throw error
+            }
+           
          }
          //Unfinished
       }
       else if(linearMatch.find())//Needs modification
       {
-         Matcher numbers = Pattern.compile("(\\p{Digit})+").matcher(func);
+         
          int numFound = 0;
-         if(numbers.find())
+         if(numsMatch.find())
          {
-            numFound = Integer.parseInt(numbers.group());
+            numFound = Integer.parseInt(numsMatch.group());
             Function f = new Constant(numFound);
          }
          
