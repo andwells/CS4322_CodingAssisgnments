760a39ffb079a2725d218eee6be04f27d80daa1d
andwells <andwells@valdosta.edu>
2015-02-09 20:33:24 -0500

    Added Pattern Matching

--- CA2_Decorator_FactoryMethod/PolynomialModel.java
+++ CA2_Decorator_FactoryMethod/PolynomialModel.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.regex.*;
 public class PolynomialModel extends Model
 {
    private int degree;
@@ -7,13 +8,31 @@ public class PolynomialModel extends Model
       degree = 0;
    }
    
-   public PolynomialModel(int deg)
-   {
-      degree = deg;
-   }
-   
    protected Function createFunction(String func)
    {
+      Pattern p = Pattern.compile("\\p{Alpha}\\^\\p{Digit}");
+      Pattern p2 = Pattern.compile("\\p{Alpha}\\^\\p{Digit}{2,}");
+      Pattern p3 = Pattern.compile("\\p{Alpha}(\\+|\\-)\\p{Digit}");
+      Pattern p4 = Pattern.compile("^\\b\\p{Digit}\\b$");
+      
+      // Matcher m = p.matcher(func);
+//       if(m.find())
+//       {
+//          //check for highest power
+//       }
+//       else if(true)//Needs modification
+//       {
+//          //check for linear
+//       }
+//       else if(true) //Needs modification
+//       {
+//          //Check for constant
+//       }
+//       else
+//       {
+//          
+//       }
+      
       char firstVar = ' ';
       ArrayList<Function> parts = new ArrayList<Function>();
 
