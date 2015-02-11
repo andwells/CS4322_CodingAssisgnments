6d9321ca3842278d5f56e57643a5c669f87e06c8
leeadams19 <leeadams19@gmail.com>
2015-02-10 20:53:11 -0500

    Create NotMath.java

---
+++ CA2_Decorator_FactoryMethod/NotMath.java
@@ -0,0 +1,29 @@
+import java.util.regex.*;
+
+public class NotMath{
+
+  public static void main(String[] args){
+
+    String problem 1 = "3x^3 + 2x^2 + x + 1";
+    String problem 2 = "20x + 15";
+    String problem 3 = "4322";
+    
+    /*CheckString(problem1);
+    CheckString(problem2);
+    CheckString(problem3);*/
+    
+  }
+  
+  /*public static void CheckString(String checkMeOut){
+  
+    if(checkMeOut.contains("^"))
+      // call evaluate for polynomial
+    else if(checkMeOut.contains(Pattern.compile("\\p{Alpha}).matcher(checkMeOut).find());
+      // call evaluate for linear
+    else
+      // call evaluate for constant
+  
+  }*/
+  
+}
+
