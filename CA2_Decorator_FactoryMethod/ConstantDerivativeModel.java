4ee5fbd73bd62ff2cc1c36c208de26e373461099
leeadams19 <leeadams19@gmail.com>
2015-02-16 14:55:14 -0500

    Create ConstantDerivativeModel.java

---
+++ CA2_Decorator_FactoryMethod/ConstantDerivativeModel.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.regex.*;
+import java.util.InputMismatchException;
+public class ConstantModel extends Model
+{
+   private int degree;
+   
+   protected Function createFunction(String func)
+   {
+      Pattern constant = Pattern.compile("^\\b(\\p{Digit})+\\b$");
+      
+      Matcher constMatch = constant.matcher(func);
+      
+      if(constMatch.find()) //Needs modification
+      {
+         return new Constant(0);
+      }
+      else
+      {
+         //throw error
+      }
+    return null;
+   }
+}
